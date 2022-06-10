dc9f2b3d42e81849a08ba1bbbc657c42b40894f1
calvinlfer <calvin.l.fer@gmail.com>
2022-06-10 14:00:31 -0400

    Update virgil to 0.9.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,5 +8,5 @@ object Versions {
   val http4s         = "0.23.12"
   val tapir          = "0.20.2"
   val sttp           = "3.6.2"
-  val virgil         = "0.9.2"
+  val virgil         = "0.9.3"
 }
