b38d3b2ed36d0bf7cfc80705875fab820ef06328
calvinlfer <calvin.l.fer@gmail.com>
2022-08-18 15:27:47 -0400

    Update dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
   val sourceCode     = "0.3.0"
   val catsEffect     = "3.3.14"
-  val fs2            = "3.2.10"
+  val fs2            = "3.2.12"
   val fs2Kafka       = "2.5.0"
   val trace4Cats     = "0.14.0"
-  val zio            = "2.0.0"
+  val zio            = "2.0.1"
   val zioInteropCats = "3.3.0"
   val zhttp          = "2.0.0-RC10"
   val http4s         = "0.23.12"
