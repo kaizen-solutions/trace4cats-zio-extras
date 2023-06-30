907637f59b612cda137b1c545ffe01924de6ced3
Eli Kasik <soujior32167@gmail.com>
2023-06-30 13:02:27 -0400

    Version bumps; new method of tracing based on LogHandler in Doobie RC4

--- doobie/src/main/scala/io/kaizensolutions/trace4cats/zio/extras/doobie/TracedTransactor.scala
+++ doobie/src/main/scala/io/kaizensolutions/trace4cats/zio/extras/doobie/TracedTransactor.scala
@@ -1,13 +1,14 @@
 package io.kaizensolutions.trace4cats.zio.extras.doobie
 
-import cats.data.Kleisli
+import cats.data.NonEmptyList
 import doobie.*
-import io.kaizensolutions.trace4cats.zio.extras.{ZSpan, ZTracer}
-import trace4cats.model.AttributeValue
+import doobie.util.log
+import io.kaizensolutions.trace4cats.zio.extras.ZTracer
+import trace4cats.model.{AttributeValue, SpanStatus}
 import zio.*
 import zio.interop.catz.*
 
-import java.sql.{Connection, PreparedStatement, ResultSet}
+import scala.concurrent.duration.FiniteDuration
 
 /**
  * A transactor that will trace all database operations performed by the
@@ -17,89 +18,53 @@ import java.sql.{Connection, PreparedStatement, ResultSet}
  * Special thanks to Eli Kasik (soujiro32167) for his help and ideas
  */
 object TracedTransactor {
-  private type Operation[A] = Kleisli[Task, PreparedStatement, A]
-
-  val layer: URLayer[ZTracer & Transactor[Task], Transactor[Task]] =
+  val layer: URLayer[ZTracer & Transactor[Task] & LogHandler[Task], Transactor[Task]] =
     ZLayer.fromZIO(
       for {
         tracer <- ZIO.service[ZTracer]
         xa     <- ZIO.service[Transactor[Task]]
-      } yield apply(xa, tracer)
+        logHander <- ZIO.service[LogHandler[Task]]
+      } yield apply(xa, tracer, logHander)
     )
 
-  def apply(underlying: Transactor[Task], tracer: ZTracer): Transactor[Task] = {
-    val tracedInterpreter: KleisliInterpreter[Task] = createKleisliInterpreter(tracer)
-    underlying.copy(interpret0 = tracedInterpreter.ConnectionInterpreter)
+  val default: URLayer[Transactor[Task] & ZTracer, Transactor[Task]] =
+    ZLayer.succeed(LogHandler.noop[Task]) >>> layer
+
+  private def tracingLogHandler(tracer: ZTracer): LogHandler[Task] = (logEvent: log.LogEvent) => {
+
+    def trace(execution: FiniteDuration, processing: Option[FiniteDuration], failure: Option[Throwable]) = {
+      tracer.withSpan(logEvent.sql.linesIterator.map(_.trim).mkString) { span =>
+        def attributes =
+          NonEmptyList.fromList(logEvent.args.map(_.toString))
+          .map(nel => "query.arguments" -> AttributeValue.StringList(nel))
+          .toMap ++
+          Map(
+            "query.label" -> AttributeValue.StringValue(logEvent.label),
+            "query.execMillis" -> AttributeValue.LongValue(execution.toMillis),
+            "query.sql" -> AttributeValue.StringValue(logEvent.sql)
+          ) ++
+          processing.map(p => "query.processingMillis" -> AttributeValue.LongValue(p.toMillis))
+
+        ZIO.when(span.isSampled)(
+          span.putAll(attributes) *>
+            ZIO.foreachDiscard(failure)(f => span.setStatus(SpanStatus.Internal(f.getMessage)))
+        ).unit
+      } *> ZIO.debug(logEvent)
+    }
+
+    logEvent match {
+      case log.Success(_, _, _, exec, processing) => trace(exec, Some(processing), None)
+      case log.ProcessingFailure(_, _, _, exec, processing, failure) => trace(exec, Some(processing), Some(failure))
+      case log.ExecFailure(_, _, _, exec, failure) => trace(exec, None, Some(failure))
+    }
   }
 
-  private def createKleisliInterpreter(tracer: ZTracer): KleisliInterpreter[Task] =
-    new KleisliInterpreter[Task] { self =>
-      override implicit val asyncM: WeakAsync[Task] = WeakAsync.doobieWeakAsyncForAsync[Task]
+  implicit class LogHandlerOps(val self: LogHandler[Task]) extends AnyVal {
+    def andThen(other: LogHandler[Task]): LogHandler[Task] = (logEvent: log.LogEvent) =>
+      self.run(logEvent) *> other.run(logEvent)
+  }
 
-      override lazy val PreparedStatementInterpreter: PreparedStatementInterpreter =
-        createPreparedStatementInterpreter(self, tracer)
-
-      override lazy val ConnectionInterpreter: ConnectionInterpreter =
-        createConnectionInterpreter(self)
-    }
-
-  private def createPreparedStatementInterpreter(
-    ki: KleisliInterpreter[Task],
-    tracer: ZTracer
-  ): ki.PreparedStatementInterpreter =
-    new ki.PreparedStatementInterpreter {
-      private def traceOperation[A](f: Operation[A]): Operation[A] =
-        Kleisli {
-          case t: TrackedPreparedStatement =>
-            val sql = formatRawSQLQuery(t.queryString)
-            tracer.withSpan(sql) { span =>
-              enrichSpanWithAttributes(span, t) *> f(t.underlying)
-            }
-
-          case other =>
-            f(other)
-        }
-
-      override val executeBatch: Operation[Array[Int]] =
-        traceOperation(super.executeBatch)
-
-      override val executeLargeBatch: Operation[Array[Long]] =
-        traceOperation(super.executeLargeBatch)
-
-      override val execute: Operation[Boolean] =
-        traceOperation(super.execute)
-
-      override val executeUpdate: Operation[Int] =
-        traceOperation(super.executeUpdate)
-
-      override val executeQuery: Operation[ResultSet] =
-        traceOperation(super.executeQuery)
-
-      override val executeLargeUpdate: Operation[Long] =
-        traceOperation(super.executeLargeUpdate)
-    }
-
-  private def createConnectionInterpreter(ki: KleisliInterpreter[Task]): ki.ConnectionInterpreter =
-    new ki.ConnectionInterpreter {
-      override def prepareStatement(rawQueryString: String): Kleisli[Task, Connection, PreparedStatement] =
-        super
-          .prepareStatement(rawQueryString)
-          .map(TrackedPreparedStatement.make(rawQueryString))
-    }
-
-  private def formatRawSQLQuery(q: String): String =
-    q.replace("\n", " ")
-      .replaceAll("\\s+", " ")
-      .trim()
-
-  private def enrichSpanWithAttributes(span: ZSpan, tps: TrackedPreparedStatement): UIO[Unit] =
-    ZIO
-      .when(span.isSampled) {
-        val parameters = tps.extractMutations.map { case (k, v) =>
-          s"?$k" -> AttributeValue.StringValue(v.toString)
-        }.toMap
-
-        span.putAll(parameters)
-      }
-      .ignoreLogged
+  def apply(underlying: Transactor[Task], tracer: ZTracer, logHandler: LogHandler[Task]): Transactor[Task] = {
+    underlying.copy(interpret0 = KleisliInterpreter(logHandler andThen tracingLogHandler(tracer)).ConnectionInterpreter)
+  }
 }
